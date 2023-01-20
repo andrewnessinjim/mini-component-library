b3142e88fe22fbd6316fbe04a5b599c8e1f38482
Andrew <andrewnessinjim@yahoo.com>
2023-01-20 22:33:02 +0530

    My progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,46 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const sizes = {
+  small: "12px",
+  medium: "18px",
+  large: "24px"
+}
+const Backdrop = styled.div`
+  width: 250px;
+  height: ${p => sizes[p.size]};
+  border-radius: 4px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: ${p => p.size === "large" && "3px"};
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${p => p.completedPercentage+"%"};
+  height: 100%;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  border-top-right-radius: ${p => p.completedPercentage >= 99 && "4px"};
+  border-bottom-right-radius: ${p => p.completedPercentage >= 99 && "4px"};
+  transition: 500ms width;
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <div>
+          <VisuallyHidden>
+            <span id='loadinglabel'>Completed percentage: </span>
+          </VisuallyHidden>
+          <Backdrop
+            role="progressbar"
+            aria-labelledby='loadinglabel'
+            aria-valuenow={value}
+            
+            size={size}>
+              <Bar 
+                completedPercentage = {value}>
+              </Bar>
+          </Backdrop>
+      </div>;
 };
 
 export default ProgressBar;
