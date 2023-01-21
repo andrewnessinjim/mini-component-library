f470640fb44707a7affa3d75f05185bbe2c48b18
Andrew <andrewnessinjim@yahoo.com>
2023-01-21 20:32:24 +0530

    My select box before watching solution

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,14 +5,64 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+
+const StyledSelect = styled.select`
+  border: none; 
+  width: 100%;
+  position: absolute;
+  inset: 0;
+  opacity: 0;
+  padding: 0;
+`;
+
+const SelectWrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  display: inline-block;
+  position: relative;
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+  &:has(${StyledSelect}:focus){
+    outline: 5px auto Highlight;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
+
+const SelectDisplayedValue = styled.p`
+  padding: 12px 24px 12px 16px;
+  display: inline-block;
+`;
+
+const IconWrapper = styled(Icon)`
+  display: inline-block;
+  padding-right: 32px;
+`
+
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+      <SelectWrapper>
+        <StyledSelect value={value} onChange={onChange}>
+          {children}
+        </StyledSelect>
+        <SelectDisplayedValue>{displayedValue}</SelectDisplayedValue>
+        <IconWrapper id="chevron-down" style={{"--stroke-width" : "2px", "--size": "18px"}}/>
+      </SelectWrapper>
   );
 };
 
 export default Select;
+
+
+{/* return (
+    
+    <SelectWrapper>
+      
+      <SelectDisplayedValue>{displayedValue}</SelectDisplayedValue>
+      <IconWrapper id="chevron-down" style={{"--stroke-width" : "2px"}}/>
+    </SelectWrapper>
+  ); */}
